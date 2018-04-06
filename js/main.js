ef74194f158a34609383cc447cd9b8c75fe60013
Larry <larry@larry.local>
2018-04-06 12:40:00 +0800

    commit 5

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const country = 'Taiwan';
+switch(grade) {
+	case 'Taiwan':
+		console.log('hello' + country);
+		break;
+	case 'Japan':
+		console.log('hello' + country);
+		break;
+	case 'Korea':
+		console.log('hello' + country);
+		break;
+	default:
+		console.log('hello' + country);		
+}
